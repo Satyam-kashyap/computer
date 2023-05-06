31a35489cfb268747b44588f398c9592d8c25f96
Satyam Kashyap <satyam.krait@gmail.com>
2023-05-07 00:29:42 +0530

    initial setup nuxt3 + vuetify3

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: [
+    "vuetify/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.css"
+  ],
+  build: {
+    transpile: [
+      "vuetify"
+    ]
+  }
+})
